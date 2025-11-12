f9e7b9e8bc1ac4f27514450bcbc8a744b842c070
Shakirat Akanji <akanjishakirato@gmail.com>
2025-11-12 22:00:22 +0100

    Scaffold a react project with vite, typescript, and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="h-dvh w-full flex items-center justify-center">
+      <p className="font-bold text-blue-500">Welcome to Bitume Task</p>
+    </div>
+  );
+};
+
+export default App;
